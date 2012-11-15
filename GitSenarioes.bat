f5ebce1419dbbf3e02653a9c1869bf628ceb85fc
Jiange Sun <jiange_sun@hotmail.com>
2012-11-15 03:13:34 -0800

    This is test perf of 10 number files

--- GitSenarioes.bat
+++ GitSenarioes.bat
@@ -5,93 +5,92 @@ set ServerPath=\\vse4001\public\v-huhuo\git
 REM==Clone TestProject for Pull
 rem git clone -n https://github.com/Jiange/TestProject.git ../%ClonePj%
 
-cd FileSizer
 rem==============
 REM Start the Git senarioes for file Size.
 
 rem=====001
-cd FileSize
+cd FileSize2
 git add file1KSize.TXT
 git commit -m "This is test perf of 1K Size file"
 
 echo This is test perf of 1K Size file>>%resultPath%
-echo %ServerPath%\timer.exe /n>>%resultPath%
+call %ServerPath%\timer.exe /n>>%resultPath%
 git push
-echo %ServerPath%\timer.exe /s>>%resultPath%
+call %ServerPath%\timer.exe /s>>%resultPath%
 
 cd ../../%ClonePj%
 echo This is test perf of 1K Size file for pull>>%resultPath%
-echo %ServerPath%\timer.exe /n>>%resultPath%
+call %ServerPath%\timer.exe /n>>%resultPath%
 git pull
-echo %ServerPath%\timer.exe /s>>%resultPath%
+call %ServerPath%\timer.exe /s>>%resultPath%
 
 rem=====002
-cd ../TestProject/FileSize
+cd ../TestProject/FileSize2
 git add file10KSize.TXT
 git commit -m "This is test perf of 10K Size file"
 
 echo This is test perf of 10K Size file>>%resultPath%
-echo %ServerPath%\timer.exe /n>>%resultPath%
+call %ServerPath%\timer.exe /n>>%resultPath%
 git push
-echo %ServerPath%\timer.exe /s>>%resultPath%
+call %ServerPath%\timer.exe /s>>%resultPath%
 
 cd ../../%ClonePj%
 echo This is test perf of 10K Size file for pull>>%resultPath%
-echo %ServerPath%\timer.exe /n>>%resultPath%
+call %ServerPath%\timer.exe /n>>%resultPath%
 git pull
-echo %ServerPath%\timer.exe /s>>%resultPath%
+call %ServerPath%\timer.exe /s>>%resultPath%
 
 rem=====003
-cd ../TestProject/FileSize
+cd ../TestProject/FileSize2
 git add file100KSize.TXT
 git commit -m "This is test perf of 100K Size file"
 
 echo This is test perf of 100K Size file>>%resultPath%
-echo %ServerPath%\timer.exe /n>>%resultPath%
+call %ServerPath%\timer.exe /n>>%resultPath%
 git push
-echo %ServerPath%\timer.exe /s>>%resultPath%
+call %ServerPath%\timer.exe /s>>%resultPath%
 
 cd ../../%ClonePj%
 echo This is test perf of 100K Size file for pull>>%resultPath%
-echo %ServerPath%\timer.exe /n>>%resultPath%
+call %ServerPath%\timer.exe /n>>%resultPath%
 git pull
-echo %ServerPath%\timer.exe /s>>%resultPath%
+call %ServerPath%\timer.exe /s>>%resultPath%
 
 rem=====004
-cd ../TestProject/FileSize
+cd ../TestProject/FileSize2
 git add file1MBSize.TXT
 git commit -m "This is test perf of 1MB Size file"
 
 echo This is test perf of 1MB Size file>>%resultPath%
-echo %ServerPath%\timer.exe /n>>%resultPath%
+call %ServerPath%\timer.exe /n>>%resultPath%
 git push
-echo %ServerPath%\timer.exe /s>>%resultPath%
+call %ServerPath%\timer.exe /s>>%resultPath%
 
 cd ../../%ClonePj%
 echo This is test perf of 1MB Size file for pull>>%resultPath%
-echo %ServerPath%\timer.exe /n>>%resultPath%
+call %ServerPath%\timer.exe /n>>%resultPath%
 git pull
-echo %ServerPath%\timer.exe /s>>%resultPath%
+call %ServerPath%\timer.exe /s>>%resultPath%
 
 rem=====005
-cd ../TestProject/FileSize
+cd ../TestProject/FileSize2
 git add file10MBSize.TXT
 git commit -m "This is test perf of 10MB Size file"
 
 echo This is test perf of 10MB Size file>>%resultPath%
-echo %ServerPath%\timer.exe /n>>%resultPath%
+call %ServerPath%\timer.exe /n>>%resultPath%
 git push
-echo %ServerPath%\timer.exe /s>>%resultPath%
+call %ServerPath%\timer.exe /s>>%resultPath%
 
 cd ../../%ClonePj%
 echo This is test perf of 10MB Size file for pull>>%resultPath%
-echo %ServerPath%\timer.exe /n>>%resultPath%
+call %ServerPath%\timer.exe /n>>%resultPath%
 git pull
-echo %ServerPath%\timer.exe /s>>%resultPath%
+call %ServerPath%\timer.exe /s>>%resultPath%
 
 REM FileNum Test==============================
 cd ../TestProject
-cd ../FileNum
+cd ../FileNum2
 
 rem=====001
 cd File10Num
@@ -99,46 +98,46 @@ git add -A
 git commit -m "This is test perf of 10 number files"
 
 echo This is test perf of 10 number files>>%resultPath%
-echo %ServerPath%\timer.exe /n>>%resultPath%
+call %ServerPath%\timer.exe /n>>%resultPath%
 git push
-echo %ServerPath%\timer.exe /s>>%resultPath%
+call %ServerPath%\timer.exe /s>>%resultPath%
 
 cd ../../../%ClonePj%
 echo This is test perf of 10 number files for pull>>%resultPath%
-echo %ServerPath%\timer.exe /n>>%resultPath%
+call %ServerPath%\timer.exe /n>>%resultPath%
 git pull
-echo %ServerPath%\timer.exe /s>>%resultPath%
+call %ServerPath%\timer.exe /s>>%resultPath%
 
 rem=====002
-cd ../TestProject/File100Num
+cd ../TestProject/FileNum2/File100Num
 git add -A
 git commit -m "This is test perf of 10 number files"
 
 echo This is test perf of 10 number files>>%resultPath%
-echo %ServerPath%\timer.exe /n>>%resultPath%
+call %ServerPath%\timer.exe /n>>%resultPath%
 git push
-echo %ServerPath%\timer.exe /s>>%resultPath%
+call %ServerPath%\timer.exe /s>>%resultPath%
 
 
 cd ../../../%ClonePj%
 echo This is test perf of 100 number files for pull>>%resultPath%
-echo %ServerPath%\timer.exe /n>>%resultPath%
+call  %ServerPath%\timer.exe /n>>%resultPath%
 git pull
-echo %ServerPath%\timer.exe /s>>%resultPath%
+call  %ServerPath%\timer.exe /s>>%resultPath%
 
 
 rem=====003
-cd ../TestProject/File1000Num
+cd ../TestProject/FileNum2/File1000Num
 git add -A
 git commit -m "This is test perf of 10 number files"
 
 echo This is test perf of 10 number files>>%resultPath%
-echo %ServerPath%\timer.exe /n>>%resultPath%
+call %ServerPath%\timer.exe /n>>%resultPath%
 git push
-echo %ServerPath%\timer.exe /s>>%resultPath%
+call %ServerPath%\timer.exe /s>>%resultPath%
 
 cd ../../../%ClonePj%
 echo This is test perf of 100 number files for pull>>%resultPath%
-echo %ServerPath%\timer.exe /n>>%resultPath%
+call %ServerPath%\timer.exe /n>>%resultPath%
 git pull
-echo %ServerPath%\timer.exe /s>>%resultPath%
+call %ServerPath%\timer.exe /s>>%resultPath%
