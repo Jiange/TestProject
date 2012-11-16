87a53beab079293611536eaa375e3f372ec13d80
Jiange Sun <jiange_sun@hotmail.com>
2012-11-16 02:33:02 -0800

    This is test perf of 10 number files

--- GitSenarioes.bat
+++ GitSenarioes.bat
@@ -1,7 +1,7 @@
 set resultPath=%1
 set ClonePj=%2
 rem timer tool path
-set ServerPath=D:\git
+set ServerPath=D:\Users\vseqa1\Documents\git
 rem \\vse4001\public\v-huhuo\git
 
 REM==Clone TestProject for Pull
